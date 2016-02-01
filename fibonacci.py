d334085c2cb61c7aceb2ab94ac3b912c8cf8518a
dcpulido <dcpulido91@gmail.com>
2016-02-01 10:31:29 +0100

    fibo

---
+++ fibonacci.py
@@ -0,0 +1,21 @@
+#fibonacci
+
+def fibonacci(x):
+	a=0
+	b=1
+	if x==0:
+		print(a)
+	elif x==1:
+		print(b)
+	else
+		print(a)
+		print(b)
+		for i in range(2,x+1):
+			aux=a+b
+			a=b
+			b=aux
+			print(aux)
+
+print("fibonacci de cuanto: ")
+z=int(input())
+fibonacci(z)
